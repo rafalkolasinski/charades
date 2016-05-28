11ab0c66bd8faf28576b24398d6d6fb5ab738e00
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2016-05-28 18:30:16 +0200

    Added bower.json & username in chat

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ io.sockets.on('connection', function(socket) {
 
 	//Send message
 	socket.on('send message', function(data) {
-		io.sockets.emit('new message', {msg: data});
+		io.sockets.emit('new message', {msg: data, user: socket.username});
 	});
 
 	//New user
