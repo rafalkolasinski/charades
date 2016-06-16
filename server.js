fd79a61b3e9d9622f08f654acb1b1063e0d7827f
Sylwia Grzegolec <s.grzegolec@gmail.com>
2016-06-16 18:01:02 +0200

    new variable for server messages for easier code reading

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var consts = require('./server_files/ServerMessagesConstant.js');
+var messagesConstants = require('./server_files/ServerMessagesConstant.js');
 var phrasesLibrary = require('./server_files/phrasesLibrary.js');
 
 var line_history = [];
@@ -12,6 +12,7 @@ var loggedInPlayers = [];
 var gameOn = false;
 
 var currentPhrase = '';
+var messages = messagesConstants.ServerMessagesConstant;
 
 server.listen(process.env.PORT || 3000);
 
@@ -23,10 +24,10 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
+io.sockets.on(messages.CONNECTION, function(socket) {
 	//Sends users array to all clients on connection
-	socket.on(consts.ServerMessagesConstant.GET_USERS, function() {
-		socket.emit(consts.ServerMessagesConstant.SEND_USERNAMES, {userlist: userNames});
+	socket.on(messages.GET_USERS, function() {
+		socket.emit(messages.SEND_USERNAMES, {userlist: userNames});
 	});
 
 	//Connect
@@ -34,40 +35,41 @@ io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
 	console.log('Connected: %s sockets connected.', connections.length);
 
 	//Disconnect
-	socket.on(consts.ServerMessagesConstant.DISCONNECT, function(data) {
+	socket.on(messages.DISCONNECT, function(data) {
 		userNames.splice(userNames.indexOf(socket.username), 1);
 		updateUsernames();
 		connections.splice(connections.indexOf(socket), 1);
+
 		if(loggedInPlayers.indexOf(socket) !== -1){
 			loggedInPlayers.splice(loggedInPlayers.indexOf(socket), 1);			
 		}
 		console.log('Disconnected: %s sockets connected.', connections.length);
 
-		if(loggedInPlayers.length < 1){
+		if(loggedInPlayers.length < 2){
 			stopGame();
 		}
 	});
 
-	socket.on(consts.ServerMessagesConstant.CHECK_LOGIN, function(data){
+	socket.on(messages.CHECK_LOGIN, function(data){
 		if(loggedInPlayers.indexOf(socket) === -1){
-			socket.emit(consts.ServerMessagesConstant.NOT_LOGGED_IN);
+			socket.emit(messages.NOT_LOGGED_IN);
 		}
 	});
 
 	//Send message
-	socket.on(consts.ServerMessagesConstant.SEND_MESSAGE, function(data) {
+	socket.on(messages.SEND_MESSAGE, function(data) {
 		console.log(data.message, currentPhrase);
 		if(data.message === currentPhrase){
-			io.sockets.emit(consts.ServerMessagesConstant.TURN_SUCCESS);
-			io.sockets.emit(consts.ServerMessagesConstant.NEW_MESSAGE, {message: data.message, username: socket.username, phrase_guessed: true});
+			io.sockets.emit(messages.TURN_SUCCESS);
+			io.sockets.emit(messages.NEW_MESSAGE, {message: data.message, username: socket.username, phrase_guessed: true});
 		}else{
-			io.sockets.emit(consts.ServerMessagesConstant.NEW_MESSAGE, {message: data.message, username: socket.username, phrase_guessed: false});
+			io.sockets.emit(messages.NEW_MESSAGE, {message: data.message, username: socket.username, phrase_guessed: false});
 		}
 
 	});
 
 	//New user
-	socket.on(consts.ServerMessagesConstant.NEW_USER, function(data) {
+	socket.on(messages.NEW_USER, function(data) {
 		socket.username = data.userName;
 		userNames.push(socket.username);
 		loggedInPlayers.push(socket);
@@ -80,38 +82,38 @@ io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
 	});
 
 	//new line handler
-	socket.on(consts.ServerMessagesConstant.DRAW_LINE, function (data) {
+	socket.on(messages.DRAW_LINE, function (data) {
     	line_history.push(data.line);
-    	io.sockets.emit(consts.ServerMessagesConstant.DRAW_LINE, { line: data.line });
+    	io.sockets.emit(messages.DRAW_LINE, { line: data.line });
    });
 
 	//czyszczenie tablicy
-	socket.on(consts.ServerMessagesConstant.CLEAR_BOARD, function () {
+	socket.on(messages.CLEAR_BOARD, function () {
 		line_history = [];
-    	io.sockets.emit(consts.ServerMessagesConstant.CLEAR_BOARD);
+    	io.sockets.emit(messages.CLEAR_BOARD);
    });
 
 	//tura jest zakończona - albo skonczl sie czas, albo ktoś zgadł
-	socket.on(consts.ServerMessagesConstant.TURN_FINISHED, function(){
+	socket.on(messages.TURN_FINISHED, function(){
 		currentPhrase = '';
 		determineNextPlayerToDraw(socket);
 	});
 
-	socket.on(consts.ServerMessagesConstant.DISMISS_TURN, function(){
+	socket.on(messages.DISMISS_TURN, function(){
 		determineNextPlayerToDraw(socket);
 	});
 
-	socket.on(consts.ServerMessagesConstant.TURN_ACCEPTED, function(){
+	socket.on(messages.TURN_ACCEPTED, function(){
 		console.log(loggedInPlayers.indexOf(socket));
 		var acceptedSocketIndex = loggedInPlayers.indexOf(socket);
 
 		currentPhrase = randomPhrase();
-		io.sockets.emit(consts.ServerMessagesConstant.TURN_START, {
+		io.sockets.emit(messages.TURN_START, {
 			userName: loggedInPlayers[acceptedSocketIndex].username,
 			phrase: currentPhrase
 		});
 
-		io.to(loggedInPlayers[acceptedSocketIndex].id).emit(consts.ServerMessagesConstant.TURN_PHRASE, {phrase: currentPhrase});
+		io.to(loggedInPlayers[acceptedSocketIndex].id).emit(messages.TURN_PHRASE, {phrase: currentPhrase});
 	})
 
 	function determineNextPlayerToDraw(socket){
@@ -125,18 +127,18 @@ io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
 			}
 			var nextUserId = loggedInPlayers[index].id;
 
-			io.to(nextUserId).emit(consts.ServerMessagesConstant.TURN_INIT);	
+			io.to(nextUserId).emit(messages.TURN_INIT);	
 		}
 	}
 
 	//Get active users
 	function updateUsernames() {
-		io.sockets.emit(consts.ServerMessagesConstant.SEND_USERNAMES, {userlist: userNames});
+		io.sockets.emit(messages.SEND_USERNAMES, {userlist: userNames});
 	}
 
 	//send already drawn lines
 	for (var i in line_history) {
-    	socket.emit(consts.ServerMessagesConstant.DRAW_LINE, { line: line_history[i] } );
+    	socket.emit(messages.DRAW_LINE, { line: line_history[i] } );
 	}
 
 	//jesli jest więcej niz 1 użytkownik (na razie) gra sie rozpoczyna
@@ -147,7 +149,7 @@ io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
 			gameOn = true;
 			currentPhrase = randomPhrase();
 
-			io.sockets.emit(consts.ServerMessagesConstant.GAME_START, {
+			io.sockets.emit(messages.GAME_START, {
 				id : loggedInPlayers[0].id.substring(2), 
 				userName: loggedInPlayers[0].username,
 				phrase: currentPhrase
@@ -158,7 +160,7 @@ io.sockets.on(consts.ServerMessagesConstant.CONNECTION, function(socket) {
 	function stopGame(){
 		currentPhrase = '';
 		gameOn = false;
-		io.sockets.emit(consts.ServerMessagesConstant.GAME_STOP);
+		io.sockets.emit(messages.GAME_STOP);
 	}
 
 	function randomPhrase(){
